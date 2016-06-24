6ec141c8a6b5a6d8c77fe48fffda5802be7a70a3
frontend <frontend.exp@gmail.com>
2016-06-24 13:44:57 +0530

    update

--- js/app.js
+++ js/app.js
@@ -48,4 +48,48 @@ $(document).ready(function() {
         stickyNav();
     });
 
+    /**Slick**/
+
+    $('.test-cont').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+    $('.country-slider').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
 });
